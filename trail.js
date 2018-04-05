ce469f606a975ffddd84deb3b7401737c6d30ca4
Иван Агафонов <i.agafonov@protonservice.ru>
2018-04-05 21:55:24 +0300

    working parser!

--- trail.js
+++ trail.js
@@ -5,163 +5,180 @@ let indent = ''
 let globalLogging = true
 const log = (toIndent, ...args) => {
   if (toIndent === false) indent = indent.slice(0, indent.length - tab.length)
-  if (globalLogging) console.log(indent, ...args)
+  if (globalLogging) {
+    args.forEach(arg => {
+      console.log(indent, arg)
+    })
+  }
   if (toIndent === true) indent += tab
 }
 
-const logger = (name, fn, params, logging) => {
-  if (logging) {
-    return function l(code) {
-      if (l.message) {
-        log(true, `parsing ${l.message} on "${code.slice(pos, pos + 15)}"`)
-        globalLogging = false
+const loggable = (name, parse, params) => {
+  return function (code) {
+    if (this.message) {
+      log(true, `parsing ${this.message}\`, \`on ${JSON.stringify(code.slice(pos, pos + 15))}`)
+      globalLogging = false
+    } else {
+      log(true, `parsing ${name}${params ? ' ' + params : ''}`, `on ${JSON.stringify(code.slice(pos, pos + 15))}`)
+    }
+    const result = parse(code)
+    if (this.message) {
+      globalLogging = true
+      if (result === undefined) {
+        log(false, `not parsed ${this.message}`)
       } else {
-        log(true, `parsing ${name}${params ? ' ' + params : ''} on "${code.slice(pos, pos + 15)}"`)
+        log(false, `parsed ${this.message} ${JSON.stringify(result)}`)
       }
-      const result = fn(code)
+    } else {
       if (result === undefined) {
         log(false, `not parsed ${name}`)
       } else {
         log(false, `parsed ${name} ${JSON.stringify(result)}`)
       }
-      if (l.message) globalLogging = true
-      return result
     }
-  } else {
-    return fn
+    return result
   }
-}
-
-const make = (name, fn, params = undefined, args) => {
-  const parser = logger(name, fn, params, true)
-  Object.defineProperty(parser, 'skp', {
-    get() {
-      parser.skip = true
-      return parser
-    }
-  })
-  Object.defineProperty(parser, 'opt', {
-    get() {
-      parser.optional = true
-      return parser
-    }
-  })
-  if (args) {
-    Object.defineProperty(parser, 'recursive', {
-      get() {
-        args.push(parser)
-        console.log(args)
-        return parser
-      }
-    })
-  }
-  parser.msg = (msg) => {
-    parser.message = msg
-    parser.params = msg
-    return parser
-  }
-  parser.transform = fn => make('transform', (code) => {
-    const result = parser(code)
-    if (result === undefined) return undefined
-    return fn(result)
-  })
-  parser.params = params
-  parser.title = name
-  return parser
 }
 
 let pos = 0
 
-const wrap = p => make('wrapper', (code) => p(code))
+const baseParserProto = Object.create(null, {
+  start: {
+    value(code) {
+      pos = 0
+      return this.parse(code)
+    }
+  },
+  skp: {
+    get() {
+      this.skip = true
+      return this
+    }
+  },
+  opt: {
+    get() {
+      this.optional = true
+      return this
+    }
+  },
+  msg: {
+    value(msg) {
+      this.message = msg
+      this.params = msg
+      return this
+    }
+  },
+  transform: {
+    value(transformFn) {
+      const parse = this.parse
+      this.parse = (code) => {
+        const result = parse.call(this, code)
+        if (result === undefined) return undefined
+        return transformFn(result)
+      }
+      return this
+    }
+  }
+})
 
-const range = (from, to) => make('range', (code) => {
+const createParser = (name, parse, params, parsers) => {
+  const parser = Object.create(baseParserProto)
+  parser.name = name
+  parser.parse = loggable(name, parse, params, parsers)
+  if (params) parser.params = params
+  if (parsers) parser.parsers = parsers
+
+  return parser
+}
+
+const range = (from, to) => createParser('range', function (code) {
   const c = code[pos]
   if (c >= from && c <= to) {
     pos += 1
     return c
   }
   return undefined
-}, `from: ${JSON.stringify(from)}, to: ${JSON.stringify(to)}`)
+}, `from: ${JSON.stringify(this.from)}, to: ${JSON.stringify(this.to)}`, undefined)
 
-const s = (str) => make('string', (code) => {
+const s = (str) => createParser('string', function (code) {
   const len = str.length
   for (let i = 0; i < len; i += 1) {
     if (code[pos + i] !== str[i]) return undefined
   }
   pos += str.length
   return str
-}, JSON.stringify(str))
+}, JSON.stringify(str), undefined)
 
-const c = (char) => make('char', (code) => {
+const c = (char) => createParser('char', function (code) {
   if (code[pos] !== char) return undefined
   pos += 1
   return char
-}, JSON.stringify(char))
+}, JSON.stringify(char), undefined)
 
-const anyChar = make('any char', (code) => {
-  const res = code[pos]
-  pos += 1
-  return res
-})
+// const anyChar = createParser('any char', function (code) {
+//   const res = code[pos]
+//   pos += 1
+//   return res
+// }, undefined, undefined)
 
 const exceptChar = (char, escape) => {
   if (escape === undefined) {
-    return make('except', (code) => {
+    return createParser('except', function (code) {
       const res = code[pos]
       if (res === char) return undefined
       pos += 1
       return res
-    }, `${JSON.stringify(char)}, escape: '${escape}'`)
+    }, `${JSON.stringify(char)}, escape: '${escape}'`, undefined)
   } else {
-    return make('except', (code) => {
+    return createParser('except', function (code) {
       const res = code[pos]
       if (res === char && code[pos - 1] !== escape) return undefined
       pos += 1
       return res
-    }, `${JSON.stringify(char)}, escape: '${escape}'`)
+    }, `${JSON.stringify(char)}, escape: '${escape}'`, undefined)
   }
 }
 
-const or = (...args) => make('or', (code) => {
-  for (const p of args) {
-    const res = p(code)
+const or = (...parsers) => createParser('or', function (code) {
+  for (const parser of parsers) {
+    const res = parser.parse(code)
     if (res === undefined) continue
     return res
   }
   return undefined
-}, `[${args.map(parser => `${parser.title}: ${parser.params}`).join(', ')}]`, args)
+}, `[${parsers.map(parser => `${parser.name}: ${parser.params}`).join(', ')}]`, parsers)
 
-const join = (...args) => make('join', (code) => {
+const join = (...parsers) => createParser('join', function (code) {
   let result = ''
-  const len = args.length
+  const len = parsers.length
   for (let i = 0; i < len; i += 1) {
-    const p = args[i]
-    const res = p(code)
+    const parser = parsers[i]
+    const res = parser.parse(code)
     if (res === undefined) {
-      if (p.optional) continue
+      if (parser.optional) continue
       return undefined
     }
-    if (!p.skip) result += res
+    if (!parser.skip) result += res
   }
   return result.length > 0 ? result : undefined
-}, `[${args.map(parser => `${parser.title}: ${parser.params}`).join(', ')}]`, args)
+}, `[${parsers.map(parser => `${parser.name}: ${parser.params}`).join(', ')}]`, parsers)
 
-const seq = (...args) => make('seq', (code) => {
+const seq = (...parsers) => createParser('seq', function (code) {
   const result = []
-  const len = args.length
+  const len = parsers.length
   const from = pos
   for (let i = 0; i < len; i += 1) {
-    const p = args[i]
-    const res = p(code)
+    const parser = parsers[i]
+    const res = parser.parse(code)
     if (res === undefined) {
-      if (p.optional) {
-        if (!p.skip) result.push(undefined)
+      if (parser.optional) {
+        if (!parser.skip) result.push(undefined)
         continue
       }
       pos = from
       return undefined
     }
-    if (!p.skip) result.push(res)
+    if (!parser.skip) result.push(res)
   }
   if (result.length > 0) {
     return result
@@ -169,21 +186,21 @@ const seq = (...args) => make('seq', (code) => {
     pos = from
     return undefined
   }
-}, `[${args.map(parser => `${parser.title}: ${parser.params}`).join(', ')}]`, args)
+}, `[${parsers.map(parser => `${parser.name}: ${parser.params}`).join(', ')}]`, parsers)
 
-const one = (...args) => make('one in seq', (code) => {
+const one = (...parsers) => createParser('one in seq', function (code) {
   const result = []
-  const len = args.length
+  const len = parsers.length
   const from = pos
   for (let i = 0; i < len; i += 1) {
-    const p = args[i]
-    const res = p(code)
+    const parser = parsers[i]
+    const res = parser.parse(code)
     if (res === undefined) {
-      if (p.optional) continue
+      if (parser.optional) continue
       pos = from
       return undefined
     }
-    if (!p.skip) result.push(res)
+    if (!parser.skip) result.push(res)
   }
   if (result.length > 0) {
     return result[0]
@@ -191,13 +208,13 @@ const one = (...args) => make('one in seq', (code) => {
     pos = from
     return undefined
   }
-}, `[${args.map(parser => `${parser.title}: ${parser.params}`).join(', ')}]`)
+}, `[${parsers.map(parser => `${parser.name}: ${parser.params}`).join(', ')}]`)
 
-const repeatChar = (p, min, max) => make('repeat char', (code) => {
+const repeatChar = (parser, min, max) => createParser('repeat char', function (code) {
   let i
   const from = pos
   for (i = 0; i < max; i += 1) {
-    const res = p(code)
+    const res = parser.parse(code)
     if (res === undefined) break
   }
   if (i < min) {
@@ -205,14 +222,14 @@ const repeatChar = (p, min, max) => make('repeat char', (code) => {
     return undefined
   }
   return code.slice(from, pos)
-}, `${JSON.stringify(p)} from ${min} times to ${max}`)
+}, `${JSON.stringify(parser)} from ${min} times to ${max}`, undefined)
 
-const repeat = (p, min, max) => make('repeat', (code) => {
+const repeat = (parser, min, max) => createParser('repeat', function (code) {
   const result = []
   let i
   const from = pos
   for (i = 0; i < max; i += 1) {
-    const res = p(code)
+    const res = parser.parse(code)
     if (res === undefined) break
     result.push(res)
   }
@@ -221,14 +238,15 @@ const repeat = (p, min, max) => make('repeat', (code) => {
     return undefined
   }
   return result
-}, `(${p.title}: ${p.params}) from ${min} times to ${max}`)
+}, `(${parser.name}: ${parser.params}) from ${min} times to ${max}`, undefined)
 
 const series = (parser, divider = c(',')) => {
   return seq(parser, repeat(one(divider.skp, WS, parser), 0, Infinity).opt).transform(([first, rest]) => ([first, ...rest]))
 }
 
 const WS = repeatChar(or(c('\u0020'), c('\u0009'), c('\u000B'), c('\u000C')), 1, Infinity).skp.msg('white space')
-const LE = repeatChar(or(c('\u000A'), c('\u000D'), s('\u000D\u000A'), c('\u2028'), c('\u2029'), 1, Infinity))
+const EOF = or(c('\u0000'), c('\u0003'))
+const LE = repeatChar(or(c('\u000A'), c('\u000D'), s('\u000D\u000A'), c('\u2028'), c('\u2029'), EOF), 1, Infinity).skp.msg('line end')
 const UPPER = range('A', 'Z')
 const LOWER = range('a', 'z')
 const DIGIT = range('0', '9')
@@ -238,50 +256,77 @@ const DOLLAR = c('$')
 
 const IDENTIFIER_FIRST_LETTER = or(UPPER, LOWER, DASH, DOLLAR)
 const IDENTIFIER_REST_LETTER = or(UPPER, LOWER, DASH, DOLLAR, DIGIT)
-const IDENTIFIER = join(IDENTIFIER_FIRST_LETTER, repeatChar(IDENTIFIER_REST_LETTER, 0, Infinity)).msg('identifier')
+const pIdentifier = join(IDENTIFIER_FIRST_LETTER, repeatChar(IDENTIFIER_REST_LETTER, 0, Infinity)).msg('identifier')
+  .transform(res => ({type: 'identifier', label: res}))
 
-const INTEGER = join(c('-').opt, NON_ZERO_DIGIT, repeatChar(DIGIT, 0, Infinity)).msg('integer')
+const pInteger = join(c('-').opt, NON_ZERO_DIGIT, repeatChar(DIGIT, 0, Infinity)).msg('integer')
+  .transform(res => ({type: 'integer', value: +res}))
 
 const STRING_SINGLE_QUOTE = join(c('\''), repeatChar(exceptChar('\'', '\\'), 0, Infinity), c('\'')).msg('single quoted string')
-  .transform(str => str.replace(/\\'/g, "'").slice(1, -1))
-const STRING_DOUBLE_QUOTE = join(c('"'), repeatChar(exceptChar('\'', '\\'), 0, Infinity), c('"')).msg('double quoted string')
+  .transform(str => str.replace(/\\'/g, '\'').slice(1, -1))
+const STRING_DOUBLE_QUOTE = join(c('"'), repeatChar(exceptChar('"', '\\'), 0, Infinity), c('"')).msg('double quoted string')
   .transform(str => str.replace(/\\"/g, '"').slice(1, -1))
-const STRING = or(STRING_SINGLE_QUOTE, STRING_DOUBLE_QUOTE).msg('string')
+const pString = or(STRING_SINGLE_QUOTE, STRING_DOUBLE_QUOTE).msg('string')
+  .transform(res => ({type: 'string', value: res}))
 
-const pImportAlias = one(s('as').skp, WS, IDENTIFIER).opt
-const pImportItem = seq(IDENTIFIER, one(WS, pImportAlias).opt)
+const pImportAlias = one(s('as').skp, WS, pIdentifier).opt
+const pImportItem = seq(pIdentifier, one(WS, pImportAlias).opt)
   .transform(([identifier, alias]) => ({identifier, alias}))
 const pImportItems = one(c('{').skp, series(pImportItem), c('}').skp)
-const pImport = seq(s('import').skp, one(WS, pImportItem).opt, one(WS, pImportItems).opt, WS, s('from').skp, WS, STRING_SINGLE_QUOTE)
-  .transform(([def, other, path]) => ({default: def, other, path}))
+const pImport = seq(s('import').skp, one(WS, pImportItem).opt, one(WS, pImportItems).opt, WS, s('from').skp, WS, pString)
+  .transform(([def, other, path]) => ({type: 'import', default: def, other, path}))
 
-// const pBinaryOperator = seq(pExpression, WS, or(c('+'), c('-'), c('*'), c('/')), WS, pExpression)
-// const pExpression = or(
-//   pLiteral,
-//   pBinaryOperator
-// )
-const pDeclaration = one(IDENTIFIER, c(':'))
-// const pStatement = or(
-//   pDeclaration,
-//   pExpression
-// )
+const pLiteral = or(
+  pInteger,
+  pString
+)
 
-const pRec = or(c('+'), c('-'))
+const pValue = or(
+  pLiteral,
+  pIdentifier
+)
+
+const pExpression = or(
+  pValue
+)
+
+const pFunctionCall = seq(pIdentifier, c('(').skp, series(pExpression), c(')'))
+  .transform(([name, args]) => ({type: 'functionCall', name, args}))
+pExpression.parsers.unshift(pFunctionCall)
+
+const pExpressionBinaryLeft = or(...pExpression.parsers)
+
+const pBinaryOperator = seq(pExpressionBinaryLeft, WS, or(c('+'), c('-'), c('*'), c('/')), WS, pExpression)
+  .transform(([left, op, right]) => ({type: 'binaryOperator', op, left, right}))
+
+pExpression.parsers.unshift(pBinaryOperator)
+
+const pDeclaration = seq(pIdentifier, c(':').skp, WS, pExpression)
+  .transform(([identifier, value]) => ({type: 'declaration', identifier, value}))
+
+const pStatement = one(
+  LE,
+  or(
+    pImport,
+    pDeclaration,
+    pExpression
+  )
+)
+
+const pModule = repeat(pStatement, 0, Infinity)
 
 const show = arg => {
-  pos = 0
   console.log('\n')
-  console.log(util.inspect(arg))
+  console.log(util.inspect(arg, false, null))
   console.log('\n')
 }
-
-show(pRec('-'))
 
 const code = `
 import {out} from 'std'
 a: 1
-b: a + 1
+b: a + 1 - 10
+'some text'
 out(b)
 `
 
-// show(pImport('import asd {out, a as b} from \'std\''))
+show(pModule.start(code))
