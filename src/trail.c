3f9b1d20b4aeb4e546208c847baa7c7f5166a7a5
Ivan Agafonov <armadil@yandex.ru>
2023-03-07 16:06:34 +0500

    some files

---
+++ src/trail.c
@@ -0,0 +1,27 @@
+typedef struct Str_ {
+    int _len;
+    char* _str;
+} Str;
+
+Str StrNew() {
+}
+
+typedef struct Logger_ {
+    void (*_logFn)(Str str);
+    Str _tab;
+} Logger;
+
+Logger LoggerNew(
+    void (*logFn)(Str str),
+    Str tab,
+    int maxPrintLines) {
+    Logger logger = {
+        ._logFn = logFn,
+        ._tab = tab};
+    return logger;
+}
+
+void main() {
+    Logger logger;
+    logger._tab
+}
